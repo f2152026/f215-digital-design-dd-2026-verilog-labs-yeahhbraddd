60f42db8cb3c4fc4c0b0e50a33651cbd0efff4f1
yeahhbraddd <f20240775@pilani.bits-pilani.ac.in>
2026-09-19 13:37:17 +0000

    lab02 task1

--- labs/lab01/task2/tb.v
+++ labs/lab01/task2/tb.v
@@ -42,4 +42,3 @@ module tb;
     $monitor($time, " a=%b b=%b cin=%b | sum=%b cout=%b", t_a, t_b, t_cin, t_sum, t_cout);
 
 endmodule
-
---
+++ labs/lab02/task1/tb.v
@@ -0,0 +1,52 @@
+// tb.v
+// Starter testbench template -- YOU complete this file.
+//
+// Goal: apply all 8 combinations of I0, I1, S (5 time units apart) to DUT
+// and observe the output. Fill in every TODO below.
+
+module tb;
+
+  // TODO: declare the three DUT inputs as the appropriate variable type.
+  // Use exactly these names: t_i0, t_i1, t_s (needed by $monitor below).
+  reg  t_i0, t_i1, t_s;
+  // TODO: declare the DUT output as the appropriate net type.
+  // Use exactly this name: t_y (needed by $monitor below).
+  wire t_y;
+
+  // TODO: instantiate DUT here, connecting t_i0, t_i1, t_s, t_y to its ports
+  DUT DUT (
+    .I0(t_i0),
+    .I1(t_i1),
+    .S (t_s),
+    .Y (t_y)
+  );
+
+
+  // Waveform dump configuration
+  string vcd_file;
+  initial begin
+    if ($value$plusargs("vcd=%s", vcd_file)) begin
+      $dumpfile(vcd_file);
+      $dumpvars(0, DUT);
+    end
+  end
+
+  initial begin
+    // TODO: apply all 8 combinations of t_i0, t_i1, t_s, 5 time units apart,
+    // then $finish. (Same pattern you used in Lab 1's tb.v.)
+    {t_s, t_i1, t_i0} = 3'b000;
+    #5 {t_s, t_i1, t_i0} = 3'b001;
+    #5 {t_s, t_i1, t_i0} = 3'b010;
+    #5 {t_s, t_i1, t_i0} = 3'b011;
+    #5 {t_s, t_i1, t_i0} = 3'b100;
+    #5 {t_s, t_i1, t_i0} = 3'b101;
+    #5 {t_s, t_i1, t_i0} = 3'b110;
+    #5 {t_s, t_i1, t_i0} = 3'b111;
+    #5 $finish;
+
+  end
+
+  initial
+    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y);
+
+endmodule
